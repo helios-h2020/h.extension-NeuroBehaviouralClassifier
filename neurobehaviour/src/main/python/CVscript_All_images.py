85c295a4ed19366ebc643264cec8a521322cb09b
Vicent Iborra - LabLENI UPV <viibba@upvnet.upv.es>
2021-03-05 13:18:47 +0100

    Changes in Python libraries and build configuration

--- neurobehaviour/src/main/python/CVscript_All_images.py
+++ neurobehaviour/src/main/python/CVscript_All_images.py
@@ -5,20 +5,19 @@ Created on Thu Feb 27 17:37:42 2020
 @author: lucarri
 """
 
-
 import os
-import matplotlib.pyplot as plt
 import cv2
-import numpy as np
 import tensorflow.keras as k
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.metrics import categorical_accuracy
 
 
 def current_path():
-    
     return os.path.dirname(os.path.realpath(__file__))
 
+def argmax(array):
+    array = list( array )
+    return array.index(max(array))
 
 #cargar el modelo:
 def baseline_model_saved(dirpath):
@@ -59,31 +58,32 @@ def faces(pic):
     #cantidad de caras
     #print("[INFO] Found {0} Faces.".format(len(faces)))
     
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    img_plot = plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    #for (x, y, w, h) in faces:
+    #    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #img_plot = plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     #plt.show()
     
     model = baseline_model_saved(dirpath)
     label_map = ['Anger', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
     
-    	#emoción de cada cara - keras:
+    #emoción de cada cara - keras:
     for (x, y, w, h) in faces:   
         roi_color = img_grey[y:y + h, x:x + w]
-        #print("[INFO] Face found.") 
-        X = np.array(roi_color[:,:])
-        X = cv2.resize(X, (48,48))
-        X = np.expand_dims(X, axis = 0)
-        X = np.expand_dims(X, axis = 3)
+        #print("[INFO] Face found.")
+        X = cv2.resize(roi_color[:,:], (48,48))
+        X = X[None, :, :]
+        X = X[:, :, :, None]
+        
         score = model.predict(X)
         #print(score)
-        plt.imshow(cv2.cvtColor(img[y:y + h, x:x + w], cv2.COLOR_BGR2RGB))
-		#busca el valor max en el array
-        plt.title(label_map[np.argmax(score)]+": " +str(np.max(score)))
+        #plt.imshow(cv2.cvtColor(img[y:y + h, x:x + w], cv2.COLOR_BGR2RGB))
+        #busca el valor max en el array
+        #plt.title(label_map[np.argmax(score)]+": " +str(np.max(score)))
         #array to store emotion and score
-        emotion.append(label_map[np.argmax(score)])
-        scoreEmotion.append(str(np.max(score)))
-		#muestra la emoción más probable con el %
+        emotion.append(label_map[argmax(score[0])])
+        scoreEmotion.append(str(max(score[0])))
+        
+        #muestra la emoción más probable con el %
         #plt.show()
             
     return len(faces), faces, emotion, scoreEmotion
